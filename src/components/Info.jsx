7ecdc4690c6661e2573c0f0731cccccf363ea251
Ameya29 <67052678+Ameya29@users.noreply.github.com>
2021-06-24 22:32:28 +0530

    Add files via upload

---
+++ src/components/Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>JavaScript & ReactJS</h1>
+      <p> A Basic Web Development ReactJS Bootcamp</p>
+    </div>
+  );
+}
+
+export default Info;
